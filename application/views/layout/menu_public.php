9eec6ce18d9384366e7c6552433235cb04c70a7c
Alaeddine Hajjem <alaeddine.hajjem@etu.utc.fr>
2016-05-16 12:36:16 +0200

    no message

--- application/views/layout/menu_public.php
+++ application/views/layout/menu_public.php
@@ -1,7 +1,7 @@
   <div class="ui centered grid">
     <div class="center aligned column">
       <div class="row">
-          <a href="<?php echo base_url().'index.php/';?>"><img class="ui fluid image" src="<?php echo base_url().'assets/images/banière.png';?>"/></a>
+          <img class="ui fluid image" src="<?php echo base_url().'assets/images/banière.png';?>"/>
       </div>
       <div class="ui menu">
           <?php foreach($familles as $famille): ?>
